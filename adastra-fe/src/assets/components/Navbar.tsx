5b5afe26f30bc4e08953cab418d80d547d45d794
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-07-31 16:40:51 +0200

    navbar mobile html & css created

--- adastra-fe/src/assets/components/Navbar.tsx
+++ adastra-fe/src/assets/components/Navbar.tsx
@@ -1,22 +1,27 @@
 import { Col, Container, Row } from "react-bootstrap"
 import type { JSX } from "react/jsx-runtime"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = (): JSX.Element => {
   return (
     <>
-      <Container fluid className="bg-black p-0">
-        <Container>
-          <Row>
-            <Col className="ps-0">
-              <img
-                src="logo_nav.png"
-                alt="logo adastra cinema"
-                style={{ maxHeight: "55px" }}
-              />
-            </Col>
-            <Col></Col>
-          </Row>
-        </Container>
+      <Container fluid className="bg-black">
+        <Row>
+          <Col className="ps-0 d-flex justify-content-between align-items-center">
+            <img
+              src="logo_nav.png"
+              alt="logo adastra cinema"
+              style={{ maxHeight: "55px" }}
+              className=" ms-3"
+            />
+            <FontAwesomeIcon
+              icon={faBars}
+              style={{ color: "#fff" }}
+              size="2xl"
+            />
+          </Col>
+        </Row>
       </Container>
     </>
   )
