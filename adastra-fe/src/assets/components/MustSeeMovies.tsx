32dcce76bcf3431b295f2b64450bda1f2455a93c
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-01 14:55:12 +0200

    slider must see ready

--- adastra-fe/src/assets/components/MustSeeMovies.tsx
+++ adastra-fe/src/assets/components/MustSeeMovies.tsx
@@ -11,9 +11,21 @@ const MustSeeMovies = () => {
           return (
             <Carousel.Item
               className="text-center"
-              key={poster.nome + " " + randomNum}
+              key={poster.name + " " + randomNum}
             >
-              <img src={poster.link} alt={poster.nome + " poster"} />
+              <img
+                className="w-100"
+                src={poster.link}
+                alt={poster.name + " poster"}
+              />
+
+              <h3 className="m-0 pt-3 text-uppercase">{poster.name}</h3>
+              <p className="m-0 date-and-time">
+                {poster.date.toLocaleDateString("it-IT") +
+                  " " +
+                  poster.time +
+                  "h"}
+              </p>
             </Carousel.Item>
           )
         })}
