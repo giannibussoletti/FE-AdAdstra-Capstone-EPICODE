1dc1e98f7d9c2d96f22e4fcceb57d217490de313
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-02 18:11:35 +0200

    Created accordion with sass edit for Footer

--- adastra-fe/src/assets/components/Footer.tsx
+++ adastra-fe/src/assets/components/Footer.tsx
@@ -1,9 +1,29 @@
-import { Container } from "react-bootstrap"
-
+import { Container, Accordion, Row, Col } from "react-bootstrap"
+import { footerInfo } from "../misc/arrays"
 const Footer = () => {
   return (
     <div className="bg-black">
-      <Container fluid="md"></Container>
+      <Container fluid="md">
+        <Row xs={1} md={2}>
+          <Col className="p-0">
+            <Accordion className="" flush alwaysOpen>
+              {footerInfo.map((info, i) => {
+                return (
+                  <Accordion.Item className="p-0 " eventKey={i.toString()}>
+                    <Accordion.Header>
+                      <h5 className="fw-normal text-uppercase mt-3">
+                        {info.title}
+                      </h5>
+                    </Accordion.Header>
+                    <Accordion.Body>{info.text}</Accordion.Body>
+                  </Accordion.Item>
+                )
+              })}
+            </Accordion>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   )
 }
