bc1fde08f454f2ee9cd7dc039f835c5951140a28
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-02 16:04:14 +0200

    footer coding

---
+++ adastra-fe/src/assets/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <div className="bg-black">
+      <Container fluid="md"></Container>
+    </div>
+  )
+}
+
+export default Footer
