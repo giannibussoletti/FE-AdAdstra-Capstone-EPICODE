5dae0b6d014f84e7e9f84997d6488877190ef54e
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-05 00:37:24 +0200

    created animation for infinite slider

--- adastra-fe/src/assets/components/PreSalesSlider.tsx
+++ adastra-fe/src/assets/components/PreSalesSlider.tsx
@@ -6,36 +6,54 @@ import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
 import MainTitles from "./MainTitles"
 import type { PropString } from "../misc/types"
 import { useState } from "react"
+import { useRef } from "react"
 const PreSalesSlider = function ({ string }: PropString) {
   const [newArrayPoster, setNewArrayPoster] = useState(arrayPoster)
+  const [animRules, setAnimRules] = useState({})
+  const posterRef = useRef<HTMLDivElement>(null)
 
   const nextslide = () => {
-    setNewArrayPoster((prevPosters) => {
-      if (prevPosters.length <= 1) return prevPosters
-
-      const newArray = [...prevPosters]
-      const firstSlide = prevPosters.shift()
-      if (firstSlide) {
-        newArray.push(firstSlide)
-        newArray.shift()
-      }
-      return newArray
+    const posterWidth = posterRef.current?.clientWidth
+    setAnimRules({
+      transform: `translateX(-${posterWidth}px)`,
+      transition: "300ms all ease-in-out",
     })
+
+    setTimeout(() => {
+      setNewArrayPoster((prevPosters) => {
+        if (prevPosters.length <= 1) return prevPosters
+
+        const [firstSlide, ...remainingSlides] = prevPosters
+        return [...remainingSlides, firstSlide]
+      })
+      setAnimRules({
+        transform: `translateX(0)`,
+        transition: "none",
+      })
+    }, 300)
   }
 
   const previousSlide = () => {
+    const posterWidth = posterRef.current?.clientWidth
+
     setNewArrayPoster((prevPosters) => {
       if (prevPosters.length <= 1) return prevPosters
-
-      const newArray = [...prevPosters]
-      const lastSlide = prevPosters.pop()
-      if (lastSlide) {
-        newArray.unshift(lastSlide)
-        newArray.pop()
-      }
-
-      return newArray
+      const lastSlide = prevPosters[prevPosters.length - 1]
+      const remainingSlides = prevPosters.slice(0, -1)
+      return [lastSlide, ...remainingSlides]
     })
+
+    setAnimRules({
+      transform: `translateX(-${posterWidth}px)`,
+      transition: "none",
+    })
+
+    setTimeout(() => {
+      setAnimRules({
+        transform: "translateX(0)",
+        transition: "300ms all ease-in-out",
+      })
+    }, 50)
   }
 
   return (
@@ -49,12 +67,17 @@ const PreSalesSlider = function ({ string }: PropString) {
       >
         {newArrayPoster.map((poster) => {
           return (
-            <SingleSlide
-              //Cambiare il tipo di key e la uniqueId
+            <div
+              className="p-0"
+              ref={posterRef}
+              style={animRules}
               key={poster.id}
-              posterLink={poster.link}
-              originalTitle={poster.name}
-            />
+            >
+              <SingleSlide
+                posterLink={poster.link}
+                originalTitle={poster.name}
+              />
+            </div>
           )
         })}
         <SliderButton
--- adastra-fe/src/assets/components/SliderComponents/SingleSlide.tsx
+++ adastra-fe/src/assets/components/SliderComponents/SingleSlide.tsx
@@ -5,7 +5,11 @@ const SingleSlide = function ({ posterLink, originalTitle }: SliderPoster) {
   // const navigate = useNavigate()
   return (
     <Col className="p-0">
-      <Image className="w-100" src={posterLink} alt={originalTitle} />
+      <Image
+        className="w-100 image-query"
+        src={posterLink}
+        alt={originalTitle}
+      />
       <h4 className="fw-normal text-uppercase mt-3">{originalTitle}</h4>
     </Col>
   )
