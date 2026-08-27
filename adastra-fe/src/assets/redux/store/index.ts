4ea805e54b8860d5f6215479eb725cdd5bf34e6b
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-27 15:01:46 +0200

    persit created an test

--- adastra-fe/src/assets/redux/store/index.ts
+++ adastra-fe/src/assets/redux/store/index.ts
@@ -1,17 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+
+import storage from "redux-persist/es/storage"
+
 import menuSlice from "../reducers/NavBarSlice"
 import bookingSlice from "../reducers/TicketSlice"
 import movieSlice from "../reducers/MovieSlice"
 import userSlice from "../reducers/UserSlice"
-export const store = configureStore({
-  reducer: {
-    menuState: menuSlice,
-    bookingState: bookingSlice,
-    movieState: movieSlice,
-    userState: userSlice,
-  },
+const rootReducer = combineReducers({
+  menuState: menuSlice,
+  bookingState: bookingSlice,
+  movieState: movieSlice,
+  userState: userSlice,
 })
 
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
--- adastra-fe/src/main.tsx
+++ adastra-fe/src/main.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import { Provider } from "react-redux"
-import { store } from "./assets/redux/store"
-
+import { store, persistor } from "./assets/redux/store"
+import { PersistGate } from "redux-persist/integration/react"
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 )
