d53ea6c10b9c8665f034f9f20a558b9daceadb94
Gianni Bussoletti <BussolettiGianni@gmail.com>
2026-08-29 19:39:44 +0200

    created required email and confimation email modal

--- adastra-fe/src/assets/components/mainPages/PaymentPage.tsx
+++ adastra-fe/src/assets/components/mainPages/PaymentPage.tsx
@@ -3,18 +3,41 @@ import TopBarInfoBooking from "../TopBarInfoBooking"
 import MainTitles from "../MainTitles"
 import Buttons from "../Buttons"
 import { useAppDispatch, useAppSelector } from "../../redux/hooks"
-import { Form, Container, Row, Col } from "react-bootstrap"
+import {
+  Form,
+  Container,
+  Row,
+  Col,
+  Popover,
+  OverlayTrigger,
+  Modal,
+  Button,
+} from "react-bootstrap"
 import { resetState } from "../../redux/reducers/TicketSlice"
 import ResponseModal from "../ResponseModal"
 import { fetchBooking } from "../../fetchs"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { blueCost, GREEN, greenCost, RED, redCost } from "../../misc/variables"
 import {
   faCircle,
   faCircleCheck,
   faCircleXmark,
+  faQuestion,
 } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const PaymentPage = () => {
+  const requiredPopover = (
+    <Popover id="requeried-popover" className=" bg-danger-subtle">
+      <Popover.Body className="text-danger-emphasis text-center">
+        <strong>
+          la mail obbligatoria <br />
+          per l'invio dei biglietti
+        </strong>
+      </Popover.Body>
+    </Popover>
+  )
+
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const [modalData, setModalData] = useState({
@@ -24,6 +47,8 @@ const PaymentPage = () => {
     style: "",
     buttonText: "",
   })
+  const [showAlert, setShowAlert] = useState(false)
+  const [showConfirmation, setShowConfirmation] = useState(false)
 
   const screenTimeId = useAppSelector(
     (state) => state.movieState.screeningTimeId,
@@ -49,6 +74,8 @@ const PaymentPage = () => {
   const [coupon, setCoupon] = useState("")
 
   const dispatch = useAppDispatch()
+  const target = useRef(null)
+
   return (
     <>
       <ResponseModal
@@ -82,9 +109,10 @@ const PaymentPage = () => {
         <Row xs={1} md={2} className="mb-3 p-0 mt-3 mt-md-0">
           <Col>
             <h5 className="f fw-normal text-uppercase mt-3 mb-2">
-              <Form.Label className="mb-0">inserisci la tua mail</Form.Label>
+              <Form.Label className="mb-0">inserisci la tua mail*</Form.Label>
             </h5>
             <Form.Control
+              ref={target}
               type="email"
               placeholder="name@example.com"
               onChange={(e) => setEmail(e.target.value)}
@@ -104,9 +132,48 @@ const PaymentPage = () => {
             <h2>totale</h2>
             <h2>{totalCost.toFixed(2)} €</h2>
           </Col>
-          <Col
-            className="text-center mt-4"
+
+          <OverlayTrigger
+            show={showAlert}
+            delay={{ show: 100, hide: 400 }}
+            placement="top"
+            overlay={requiredPopover}
+          >
+            <Col className="text-center mt-4">
+              <Button
+                onClick={() => {
+                  if (guestEmail) {
+                    setShowConfirmation(true)
+                  } else {
+                    setShowAlert(true)
+                  }
+                }}
+                variant="buttons"
+                className="rounded-pill fw-semibold text-uppercase py-2"
+              >
+                <span className="mx-3">Acquista</span>
+              </Button>
+            </Col>
+          </OverlayTrigger>
+        </Row>
+      </Container>
+
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <Modal show={showConfirmation} backdrop="static" keyboard={false}>
+        <Modal.Header>
+          <Modal.Title as={"h5"}>Richiesta di conferma</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>L'email inserita è corretta?</p>
+          <strong>{guestEmail}</strong>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="success"
             onClick={() => {
+              setShowConfirmation(false)
               fetchBooking(
                 screenTimeId,
                 maxSeats,
@@ -137,10 +204,18 @@ const PaymentPage = () => {
                 })
             }}
           >
-            <Buttons string="acquista" />
-          </Col>
-        </Row>
-      </Container>
+            Confermo
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => {
+              setShowConfirmation(false)
+            }}
+          >
+            C'è un errore
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
