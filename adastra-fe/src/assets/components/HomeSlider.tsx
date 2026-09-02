4eeb4c07ec8f1d2c98323fb10ee532b464a367cc
giannibussoletti <bussolettigianni@gmail.com>
2026-09-02 21:48:35 +0200

    updated homepage slider with real image

--- adastra-fe/src/assets/components/HomeSlider.tsx
+++ adastra-fe/src/assets/components/HomeSlider.tsx
@@ -1,23 +1,55 @@
-import { Carousel, Container, Row, Col } from "react-bootstrap"
-// TODO da cancellare una volta fatta la fetch per richiamare le immagini dal DB
-import { arrayMapImg } from "../temp"
+import { Carousel, Container, Row, Col, Button } from "react-bootstrap"
+import { useAppSelector } from "../redux/hooks"
 
 const HomeSlider = () => {
+  const movies = useAppSelector((state) => state.movieState.allMovies)
+  const banners = movies
+    .filter((movie) => movie.bannerLink !== null)
+    .flatMap((movie) => {
+      return {
+        link: movie.bannerLink,
+        movieId: movie.id,
+        movieName: movie.title,
+        tagline: movie.tagline,
+      }
+    })
+  console.log(banners)
   return (
-    <Container className="p-0">
-      <Row>
-        <Col className="p-0">
-          <Carousel controls={false} indicators={false} className="mt-4">
-            {arrayMapImg.map((img) => {
-              return (
-                <Carousel.Item key={img.id} interval={5000}>
-                  <img className="w-100 rounded-4" src={img.slide} alt="" />
-                </Carousel.Item>
-              )
-            })}
-          </Carousel>
-        </Col>
-      </Row>
+    <Container fluid className="p-0 bg-text-fields">
+      <Container>
+        <Row>
+          <Col className="p-0">
+            <Carousel controls={false} indicators={false} className="mt-4">
+              {banners.map((img) => {
+                return (
+                  <Carousel.Item
+                    key={img.movieId}
+                    interval={5000}
+                    style={{ maxWidth: "1440px", maxHeight: "480px" }}>
+                    <img
+                      className="w-100"
+                      style={{ objectFit: "cover" }}
+                      src={img.link}
+                      alt={img.movieName + " poster"}
+                    />
+                    <Carousel.Caption>
+                      <Row>
+                        <Col>
+                          <h2 className="mb-1 mt-2 text-uppercase">{img.movieName}</h2>
+                          <p>{img.tagline}</p>
+                        </Col>
+                        <Col xs="auto">
+                          <Button>Ciao</Button>
+                        </Col>
+                      </Row>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                )
+              })}
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   )
 }
