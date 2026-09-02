94350245e22e9eda07a91554bf756bf7f9bd5c00
giannibussoletti <bussolettigianni@gmail.com>
2026-09-02 22:17:19 +0200

    added golden border to start time

--- adastra-fe/src/assets/components/MovieStartTime.tsx
+++ adastra-fe/src/assets/components/MovieStartTime.tsx
@@ -19,31 +19,33 @@ const MovieStartTime = ({
   const prevTimeId = useAppSelector((state) => state.movieState.screeningTimeId)
 
   return (
-    <div
-      className=" bg-time-color rounded rounded-3 px-3 py-2 single-movie-time"
-      onClick={() => {
-        if (screeningTimeId !== prevTimeId) {
-          dispatch(
-            movieChoice({
-              movieID,
-              movieTitle,
-              duration,
-              screen,
-              screenId,
-              screeningTimeId,
-              date,
-            }),
-          )
-          dispatch(resetState())
-        }
-        navigate("/scelta-posto/")
-      }}>
-      <p className="m-0 fw-semibold fs-6">{calculateMovieTime(date, duration)}</p>
-      <p className="text-subtext-time fst-italic fw-medium">sala {screen}</p>
-      <p className="text-end m-0">
-        <span className="text-subtext-time fs-6">da </span>
-        <span className="fw-semibold fs-6">4,99€</span>
-      </p>
+    <div className="golden-border">
+      <div
+        className="px-3 py-2 single-movie-time "
+        onClick={() => {
+          if (screeningTimeId !== prevTimeId) {
+            dispatch(
+              movieChoice({
+                movieID,
+                movieTitle,
+                duration,
+                screen,
+                screenId,
+                screeningTimeId,
+                date,
+              }),
+            )
+            dispatch(resetState())
+          }
+          navigate("/scelta-posto/")
+        }}>
+        <p className="m-0 fw-semibold fs-6">{calculateMovieTime(date, duration)}</p>
+        <p className="fst-italic fw-medium">sala {screen}</p>
+        <p className="text-end m-0">
+          <span className="fs-6">da </span>
+          <span className="fw-semibold fs-6">4,99€</span>
+        </p>
+      </div>
     </div>
   )
 }
